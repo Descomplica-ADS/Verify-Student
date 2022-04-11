985945dda2cd8b4a3f795a1505398ebb81dd7a24
Vinicius <suburbanizar@gmail.com>
2022-04-10 22:29:18 -0300

    :art: feat: Improve structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { db } from "./database/index.js";
+import { db } from "./database/db.js";
 import { client } from "./discord/connection.js";
-import { validateRA } from "./utils/index.js";
+import { validateRA } from "./utils/regex.js";
 import roles from "./utils/roles.js";
 
 client.on("messageCreate", async (message) => {
@@ -14,12 +14,12 @@ client.on("messageCreate", async (message) => {
     if (!RA) return message.reply("Digite o número do seu RA. :pencil:");
 
     if (!validateRA(RA))
-      return message.reply("Número de RA inválido ! :no_entry_sign:");
+      return message.reply("Número de RA inválido! :no_entry_sign:");
 
     const ra_number = Number(RA.replace("-", ""));
     const existsRA = await db("RAs").select().where({ ra_number });
     if (existsRA.length > 0) {
-      return message.reply("Este RA já esta verificado.");
+      return message.reply("Este RA já está verificado.");
     }
     const rows = await db("RAs").select().where({ user_id });
     if (rows.length > 0) {
@@ -29,8 +29,7 @@ client.on("messageCreate", async (message) => {
     await db("RAs").insert({ ra_number, username, user_id });
 
     message.reply(
-      `${author} você está verificado com o RA ${RA} ! :white_check_mark:`,
-      message.member.roles.add(roleID)
+      `${author} Você foi verificado com o RA${RA}! :white_check_mark:`,
     );
     message.member.roles.add(roles.verified);
   }
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -2,5 +2,3 @@ export const validateRA = (RA) => {
   const regex = /^221\d{4}-006/;
   return regex.test(RA);
 };
-
-export const roleID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
