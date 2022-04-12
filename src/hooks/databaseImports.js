9ed7d7139ed2ed4033437602c979eede7e0a02ff
NyctibiusVII <matheus.dev.07@gmail.com>
2022-04-11 23:15:02 -0300

    ♻️ refactor: Organizing Imports

---
+++ src/hooks/databaseImports.js
@@ -0,0 +1,10 @@
+import { db }                   from "../database/db.js"
+import { userVerified }         from "../database/insert.js"
+import { existsUser, existsRA } from "../database/query.js"
+
+export {
+    db,
+    userVerified,
+    existsUser,
+    existsRA,
+}
---
+++ src/hooks/discordImports.js
@@ -0,0 +1,10 @@
+import { Client, Intents } from "discord.js"
+import { connClient } from "../discord/connection.js"
+import { roles }      from "../utils/roles.js"
+
+export {
+    Client,
+    Intents,
+    connClient,
+    roles
+}
