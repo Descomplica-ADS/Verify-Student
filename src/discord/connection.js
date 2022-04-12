a01eb3f28140645bf099b33de13dea983944c380
NyctibiusVII <matheus.dev.07@gmail.com>
2022-04-11 23:17:44 -0300

    :sparkles: refactor: Set configs

--- src/discord/connection.js
+++ src/discord/connection.js
@@ -1,24 +1,27 @@
-import { Client, Intents } from "discord.js";
+import { Client, Intents } from "../hooks/discordImports.js"
+import "dotenv/config"
 
-export const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS,
-  ],
-});
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+    ],
+})
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setStatus("dnd");
-  client.user.setPresence({
-    activities: [
-      {
-        name: "Aula do Ubira 🧑‍💻",
-        type: "WATCHING",
-      },
-    ],
-  });
-});
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setStatus("online")
+    client.user.setPresence({
+        activities: [
+            {
+                name: "Aula do Ubira 🧑‍💻",
+                type: "WATCHING",
+            },
+        ],
+    })
+})
 
-client.login(process.env.token);
+client.login(process.env.TOKEN)
+
+export { client as connClient }
