3d77a53db4bc9bcbe7f11d41143f7a61f9e369fc
NyctibiusVII <matheus.dev.07@gmail.com>
2022-04-11 23:11:03 -0300

    ♻️ refactor: New regex

---
+++ src/hooks/regexImports.js
@@ -0,0 +1,13 @@
+import {
+    validadeCodeAndRA,
+    validateNamedCode,
+    validateAllCodes,
+    validateRA
+} from "../utils/regex.js"
+
+export {
+    validadeCodeAndRA,
+    validateNamedCode,
+    validateAllCodes,
+    validateRA
+}
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,4 +1,11 @@
-export const validateRA = (RA) => {
-  const regex = /^221\d{4}-006/;
-  return regex.test(RA);
-};
+const
+    regexCodeAndRA = /!verif((i)(ca(r?|tion)|que)|y) +((2)\6)1\d{4}-((0)\8)6/,
+    regexCode      = /!verif((i)(ca(r?|tion)|que)|y)/,
+    regexAllCodes  = /^![a-zA-Z]+/,
+    regexRA        = /((2)\2)1\d{4}-((0)\4)6/
+
+export const
+    validadeCodeAndRA = content => regexCodeAndRA.test(content),
+    validateNamedCode = code    => regexCode.test(code),
+    validateAllCodes  = code    => regexAllCodes.test(code),
+    validateRA        = RA      => regexRA.test(RA)
